c8796fc75d2c22547cd69badee87feda6ad837c1
huanglei <1642331764@qq.com>
2022-02-28 15:28:59 +0800

    2.28 commit castle可扩展性与框架加数据

--- castle/src/castle/Game.java
+++ castle/src/castle/Game.java
@@ -1,12 +1,20 @@
 package castle;
 
+import jdk.internal.org.objectweb.asm.Handle;
+import sun.awt.SunHints;
+
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class Game {
     private Room currentRoom;
+    private HashMap<String,Handler> handlers=new HashMap<String, Handler>();
         
     public Game() 
     {
+        handlers.put("go",new HandlerGo(this));
+        handlers.put("bye", new HandlerBye(this));
+        handlers.put("help",new HandlerHelp(this));
         createRooms();
     }
 
@@ -22,11 +30,17 @@ public class Game {
         bedroom = new Room("卧室");
         
         //	初始化房间的出口
-        outside.setExits(null, lobby, study, pub);
-        lobby.setExits(null, null, null, outside);
-        pub.setExits(null, outside, null, null);
-        study.setExits(outside, bedroom, null, null);
-        bedroom.setExits(null, null, null, study);
+        outside.setExit("east", lobby);
+        outside.setExit("south",study);
+        outside.setExit("west",pub);
+
+        lobby.setExit("east", lobby);
+        pub.setExit("east", outside);
+        study.setExit("north",outside);
+        study.setExit("east",bedroom);
+        bedroom.setExit("west",study);
+        lobby.setExit("up",pub);
+        pub.setExit("down",lobby);
 
         currentRoom = outside;  //	从城堡门外开始
     }
@@ -42,17 +56,9 @@ public class Game {
 
     // 以下为用户命令
 
-    private void printHelp() 
-    {
-        System.out.print("迷路了吗？你可以做的命令有：go bye help");
-        System.out.println("如：\tgo east");
-    }
-
-    private void goRoom(String direction) 
+    public void goRoom(String direction)
     {
         Room nextRoom = currentRoom.getExit(direction);
-
-
         if (nextRoom == null) {
             System.out.println("那里没有门！");
         }
@@ -68,26 +74,37 @@ public class Game {
         System.out.println(currentRoom.getExitDesc());
         System.out.println();
     }
+    public void play(){
+        Scanner in = new Scanner(System.in);
+        while ( true ) {
+            String line = in.nextLine();
+            String[] words = line.split(" ");
+            Handler handler=handlers.get(words[0]);
+            String value="";
+            if(words.length>1)
+                value=words[1];
+            if(handler!=null){
+                handler.doCmd(value);
+                if(handler.isBye())
+                    break;
+            }
+
+//            if ( words[0].equals("help") ) {
+//                printHelp();
+//            } else if (words[0].equals("go") ) {
+//                goRoom(words[1]);
+//            } else if ( words[0].equals("bye") ) {
+//                break;
+//            }
+        }
+        in.close();
+    }
 
 	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in);
 		Game game = new Game();
 		game.printWelcome();
-
-        while ( true ) {
-        		String line = in.nextLine();
-        		String[] words = line.split(" ");
-        		if ( words[0].equals("help") ) {
-        			game.printHelp();
-        		} else if (words[0].equals("go") ) {
-        			game.goRoom(words[1]);
-        		} else if ( words[0].equals("bye") ) {
-        			break;
-        		}
-        }
-        
+		game.play();
         System.out.println("感谢您的光临。再见！");
-        in.close();
 	}
 
 }
--- castle/src/castle/Room.java
+++ castle/src/castle/Room.java
@@ -1,28 +1,19 @@
 package castle;
 
+import java.util.HashMap;
+
 public class Room {
     private String description;
-    private Room northExit;
-    private Room southExit;
-    private Room eastExit;
-    private Room westExit;
+    private HashMap<String,Room> exits=new HashMap<String, Room>();
 
+    public void setExit(String dir,Room room){
+        exits.put(dir, room);
+    }
     public Room(String description) 
     {
         this.description = description;
     }
 
-    public void setExits(Room north, Room east, Room south, Room west) 
-    {
-        if(north != null)
-            northExit = north;
-        if(east != null)
-            eastExit = east;
-        if(south != null)
-            southExit = south;
-        if(west != null)
-            westExit = west;
-    }
 
     @Override
     public String toString()
@@ -32,32 +23,15 @@ public class Room {
 
     public String getExitDesc(){
         StringBuffer sb=new StringBuffer();
-        if(northExit!=null)
-            sb.append("north");
-        if(eastExit!=null)
-            sb.append("east");
-        if(westExit!=null)
-            sb.append("west");
-        if(southExit!=null)
-            sb.append("south");
+        for(String dir:exits.keySet()){
+            sb.append(dir);
+            sb.append(" ");
+        }
         return sb.toString();
     }
 
     public Room getExit(String direction){
-        Room ret=null;
-        if(direction.equals("north")) {
-            ret = northExit;
-        }
-        if(direction.equals("east")) {
-            ret = eastExit;
-        }
-        if(direction.equals("south")) {
-            ret = southExit;
-        }
-        if(direction.equals("west")) {
-            ret = westExit;
-        }
-        return ret;
+        return exits.get(direction);
 
     }
 }
