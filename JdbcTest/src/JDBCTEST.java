e970418fe3d631896fee4d650236226d9c82945a
huanglei <1642331764@qq.com>
2022-03-25 21:12:04 +0800

    3.25 commit

--- JdbcTest/src/JDBCTEST.java
+++ JdbcTest/src/JDBCTEST.java
@@ -3,16 +3,16 @@ import java.sql.DriverManager;
 import java.sql.Statement;
 
 public class JDBCTEST {
-    public static void main(String[] args) throws ClassNotFoundException {
+    public static void main(String[] args) throws Exception {
 //        1.注册驱动
-        Class.forName("com.mysql.jdbc.Driver");
+        Class.forName("com.mysql.cj.jdbc.Driver");
 //        2.获取连接
-        String url="jdbc:mysql://127.0.0.1:3306/";
+        String url="jdbc:mysql://localhost:3306";
         String username="root";
-        String password="1234";
+        String password="123456";
         Connection conn=DriverManager.getConnection(url,username,password);
 //        3.定义sql
-        String sql="update account set money = 2000 where id = 1";
+        String sql="UPDATE school.student SET `性别` = 1 WHERE `学号`=0004";
 //         4.获取执行sql的对象Statement
         Statement stmt = conn.createStatement();
 //        5.执行sql
