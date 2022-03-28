c209a40beb2967bad6fe946af58b4030a125cd28
huanglei <1642331764@qq.com>
2022-03-28 20:02:39 +0800

    3.28 commit

--- RPC/src/Client.java
+++ RPC/src/Client.java
@@ -1,8 +1,9 @@
 public class Client {
     public static void main(String[] args) {
-        IUserService service = (IUserService) Stub.getStub(IUserService.class);
-        IProductService service2 = (IProductService) Stub.getStub(IProductService.class);
-        System.out.println(service.findUserById(123));
-        System.out.println(service2.findProductByName("Bob"));
+        System.out.println(new IUserServiceImpl().findUserById(123));
+//        IUserService service = (IUserService) Stub.getStub(IUserService.class);
+//        IProductService service2 = (IProductService) Stub.getStub(IProductService.class);
+//        System.out.println(service.findUserById(123));
+//        System.out.println(service2.findProductByName("Bob"));
     }
 }
