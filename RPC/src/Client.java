0c7a25fbf9b6a25ffba507ad1f1c0b71fc7b49b7
huanglei <1642331764@qq.com>
2022-04-03 22:55:27 +0800

    4.3 commit

--- RPC/src/Client.java
+++ RPC/src/Client.java
@@ -1,6 +1,6 @@
 public class Client {
     public static void main(String[] args) throws Exception {
-//        System.out.println(new IUserServiceImpl().findUserById(123));
+//        System.out.println(new IUserServiceImpl().findUserById(1));
         IUserService service = (IUserService) Stub.getStub(IUserService.class);
         IProductService service2 = (IProductService) Stub.getStub(IProductService.class);
         System.out.println(service.findUserById(1));
--- RPC/src/IUserService.java
+++ RPC/src/IUserService.java
@@ -1,4 +1,3 @@
-import java.sql.SQLException;
 
 public interface IUserService {
     User findUserById(int id) throws Exception;
