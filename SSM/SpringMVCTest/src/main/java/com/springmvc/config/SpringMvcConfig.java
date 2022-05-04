490abb3213cd4bccd0fec77eebb3134b8be576ca
huanglei <1642331764@qq.com>
2022-05-04 20:00:26 +0800

    5.4 commit

--- SSM/SpringMVCTest/src/main/java/com/springmvc/config/SpringMvcConfig.java
+++ SSM/SpringMVCTest/src/main/java/com/springmvc/config/SpringMvcConfig.java
@@ -1,11 +1,23 @@
 package com.springmvc.config;
 
+import com.springmvc.controller.interceptor.ProjectInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @ComponentScan ("com.springmvc.controller")
 @EnableWebMvc //Json数据支持
-public class SpringMvcConfig {
+public class SpringMvcConfig implements WebMvcConfigurer {
+
+    @Autowired
+    private ProjectInterceptor projectInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(projectInterceptor).addPathPatterns("/user/users/*","/user/*");
+    }
 }
--- SSM/SpringMVCTest/src/main/java/com/springmvc/dao/UserDao.java
+++ SSM/SpringMVCTest/src/main/java/com/springmvc/dao/UserDao.java
@@ -3,6 +3,5 @@ package com.springmvc.dao;
 import com.springmvc.domain.User;
 
 public interface UserDao {
-
     public void save(User user);
 }
