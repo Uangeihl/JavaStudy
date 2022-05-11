b4327269d40e7829c25c8a952c2e06c11c108fc4
huanglei <1642331764@qq.com>
2022-05-11 17:19:39 +0800

    5.11 commit

--- SSM/Ssm/src/main/java/com/ssm/service/impl/BookServiceImpl.java
+++ SSM/Ssm/src/main/java/com/ssm/service/impl/BookServiceImpl.java
@@ -1,7 +1,10 @@
 package com.ssm.service.impl;
 
+import com.ssm.controller.Code;
 import com.ssm.dao.BookDao;
 import com.ssm.domain.Book;
+import com.ssm.exception.BusinessException;
+import com.ssm.exception.SystemException;
 import com.ssm.service.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -34,6 +37,16 @@ public class BookServiceImpl implements BookService {
 
     @Override
     public Book getById(Integer id) {
+
+        if(id==1){
+            throw new BusinessException(Code.BUSINESS_ERR,"格式不规范");
+        }
+        //将可能出现的异常进行包装，转换成自定义异常
+        try {
+            int i = 1/0;
+        }catch (ArithmeticException e) {
+            throw new SystemException(Code.SYSTEM_TIMEOUT_ERR,"服务器访问超时，请重试！",e);
+        }
         return bookDao.getById(id);
     }
 
